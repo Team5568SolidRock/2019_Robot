b0780d60cee9c71dccaee89007e01b545f6c261c
Max Green <mtg.greenfam@gmail.com>
2019-01-07 22:14:33 -0600

    Basic Tank Drive w/ Talons

--- 2019_Robot/src/main/java/frc/robot/Robot.java
+++ 2019_Robot/src/main/java/frc/robot/Robot.java
@@ -8,14 +8,45 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends TimedRobot {
+
+  //Create Joysticks
+  Joystick m_joystick_left;
+  Joystick m_joystick_right;
+
+  // Create Drive Motors
+  Talon m_left_front;
+  Talon m_right_front;
+  Talon m_left_back;
+  Talon m_right_back;
+
+  // Initialize Drive Motors
+  DifferentialDrive m_drive;
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
   @Override
   public void robotInit() {
+
+    // Initialize Joysticks
+    m_joystick_left = new Joystick(0);
+    m_joystick_right = new Joystick(1);
+
+    // Initialize Drive Motors
+    m_left_front = new Talon(0);
+    m_right_front = new Talon(1);
+    m_left_back = new Talon(2);
+    m_right_back = new Talon(3);
+
+    // Initialize Drive Object
+    m_drive = new DifferentialDrive(m_left_front, m_right_front);
+
   }
 
   /**
@@ -46,6 +77,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    m_drive.tankDrive(m_joystick_right.getY(), m_joystick_left.getY());
   }
 
   /**
