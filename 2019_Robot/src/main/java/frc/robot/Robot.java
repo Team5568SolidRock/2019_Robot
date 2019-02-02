6dded7ffd97b5f416a2fe01ff82fb734b7add136
WolfBytes <ftcwolfbytes10436@gmail.com>
2019-02-02 16:43:22 -0600

    Working Line Follow + Arduino File

--- 2019_Robot/src/main/java/frc/robot/Robot.java
+++ 2019_Robot/src/main/java/frc/robot/Robot.java
@@ -15,8 +15,6 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.I2C;
-//import edu.wpi.first.wpilibj.I2C.Port;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -56,6 +54,8 @@ public class Robot extends TimedRobot {
 
   Vision Other;
 
+  int Error;
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -69,16 +69,18 @@ public class Robot extends TimedRobot {
     m_gamepad = new Joystick(2);
 
     // Initialize Drive Motors
-    m_left_front = new TalonSRX(2);
-    m_right_front = new TalonSRX(1);
-    m_left_back = new VictorSPX(3);
-    m_right_back = new VictorSPX(4);
+    m_left_front = new TalonSRX(7);
+    m_right_front = new TalonSRX(9);
+    m_left_back = new VictorSPX(6);
+    m_right_back = new VictorSPX(8);
 
     // Serial Port Initialize
     m_arduino = new SerialPort(9600, Port.kUSB);
     m_arduino.setReadBufferSize(1);
 
     // Configure Victors
+    m_right_front.setInverted(true);
+    m_right_back.setInverted(true);
     m_left_back.follow(m_left_front);
     m_right_back.follow(m_right_front);
 
@@ -92,6 +94,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    ArduinoDrive();
   }
 
   /**
@@ -117,8 +120,26 @@ public class Robot extends TimedRobot {
       tank_Drive(m_gamepad.getRawAxis(1), m_gamepad.getRawAxis(5), m_left_front, m_right_front);
     }
     else {
-      //Other.testPixy1();
-      ArduinDrive();
+      if(Error > 0)
+      {
+        m_left_front.set(ControlMode.PercentOutput, .3);
+        m_right_front.set(ControlMode.PercentOutput, .2);
+      }
+      else if(Error < 0)
+      {
+        m_left_front.set(ControlMode.PercentOutput, .2);
+        m_right_front.set(ControlMode.PercentOutput, .3);
+      }
+      else if(Error == 0)
+      {
+        m_left_front.set(ControlMode.PercentOutput, .2);
+        m_right_front.set(ControlMode.PercentOutput, .2);
+      }
+      else
+      {
+        m_left_front.set(ControlMode.PercentOutput, 0);
+        m_right_front.set(ControlMode.PercentOutput, 0);
+      }
     }
   }
 
@@ -137,7 +158,7 @@ public class Robot extends TimedRobot {
     // Square joystick values
     double updated_left = -joystick_left_y * Math.abs(joystick_left_y);
 
-    double updated_right = joystick_right_y * Math.abs(joystick_right_y);
+    double updated_right = -joystick_right_y * Math.abs(joystick_right_y);
 
     // Set left values
     motor_left.set(ControlMode.PercentOutput, updated_left);
@@ -146,18 +167,17 @@ public class Robot extends TimedRobot {
     motor_right.set(ControlMode.PercentOutput, updated_right);
   }
 
-  private void ArduinDrive(TalonSRX drive_left, TalonSRX drive_right)
+  private void ArduinoDrive()
   {
-    byte[] output = m_arduino.read(1);
-    System.out.println(output[0]);
-
-    int error = output[0];
-
-    if(error > 0)
+    byte[] output = m_arduino.read(m_arduino.getBytesReceived());
+    if(output.length == 0)
     {
-      drive_left.set(ControlMode.PercentOutput, .2);
-      drive_right.set(ControlMode.PercentOutput, 0);
+      Error = 0;
     }
-    
+    else
+    {
+      Error = output[output.length-1];
+      System.out.println(Error);
+    }
   }
 }
