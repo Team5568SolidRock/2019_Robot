43841a81249db6837d35818448e1504d414c3cfe
Max Green <mtg.greenfam@gmail.com>
2019-02-04 20:16:12 -0600

    Major Drive Changes and Classes

--- 2019_Robot/src/main/java/frc/robot/Robot.java
+++ 2019_Robot/src/main/java/frc/robot/Robot.java
@@ -7,54 +7,31 @@
 
 package frc.robot;
 
-import frc.robot.vision.Vision;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.SerialPort.Port;
-import edu.wpi.first.wpilibj.DigitalInput;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import java.lang.Thread;
+import frc.robot.classes.PixyLineFollow;
+import frc.robot.classes.TankDrive;
 
 public class Robot extends TimedRobot {
-  //Create and Initialize I2C Ports & MAX_BYTES
-  //private static I2C Wire = new I2C(Port.kOnboard, 4);
-	private static final int MAX_BYTES = 32;
 
   //Create Joysticks
-  Joystick m_joystick_left;
-  Joystick m_joystick_right;
+  Joystick m_joystickLeft;
+  Joystick m_joystickRight;
   Joystick m_gamepad;
 
   // Create Drive Motors
-  TalonSRX m_left_front;
-  TalonSRX m_right_front;
-  VictorSPX m_left_back;
-  VictorSPX m_right_back;
+  TalonSRX m_leftFront;
+  TalonSRX m_rightFront;
+  VictorSPX m_leftBack;
+  VictorSPX m_rightBack;
 
-  // Create Configurable Values
-  NetworkTableEntry m_deadzone;
-
-  // Create SerialPort connection
-  SerialPort m_arduino;
-
-  Double drive_left = 0.;
-  Double drive_right = 0.;
-
-  Thread Pixy;
-
-  DigitalInput Arduino12 = new DigitalInput(0);
-  DigitalInput Arduino13 = new DigitalInput(1);
-
-  Vision Other;
-
-  int Error;
+  // Create Custom Classes
+  PixyLineFollow m_pixy;
+  TankDrive m_drive;
 
   /**
    * This function is run when the robot is first started up and should be
@@ -64,27 +41,25 @@ public class Robot extends TimedRobot {
   public void robotInit() {
 
     // Initialize Joysticks
-    m_joystick_left = new Joystick(0);
-    m_joystick_right = new Joystick(1);
+    m_joystickLeft = new Joystick(0);
+    m_joystickRight = new Joystick(1);
     m_gamepad = new Joystick(2);
 
     // Initialize Drive Motors
-    m_left_front = new TalonSRX(7);
-    m_right_front = new TalonSRX(9);
-    m_left_back = new VictorSPX(6);
-    m_right_back = new VictorSPX(8);
+    m_leftFront = new TalonSRX(7);
+    m_rightFront = new TalonSRX(9);
+    m_leftBack = new VictorSPX(6);
+    m_rightBack = new VictorSPX(8);
 
-    // Serial Port Initialize
-    m_arduino = new SerialPort(9600, Port.kUSB);
-    m_arduino.setReadBufferSize(1);
+    // Initialize Custom Classes
+    m_pixy = new PixyLineFollow();
+    m_drive = new TankDrive(m_leftFront, m_rightFront);
 
     // Configure Victors
-    m_right_front.setInverted(true);
-    m_right_back.setInverted(true);
-    m_left_back.follow(m_left_front);
-    m_right_back.follow(m_right_front);
-
-    Other = new Vision();
+    m_rightFront.setInverted(true);
+    m_rightBack.setInverted(true);
+    m_leftBack.follow(m_leftFront);
+    m_rightBack.follow(m_rightFront);
   }
 
   /**
@@ -94,21 +69,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    ArduinoDrive();
-  }
-
-  /**
-   * This function is called when autonomous is first started.
-   */
-  @Override
-  public void autonomousInit() {
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
+    m_pixy.arduinoRead();
   }
 
   /**
@@ -117,67 +78,10 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     if(!m_gamepad.getRawButton(1)){
-      tank_Drive(m_gamepad.getRawAxis(1), m_gamepad.getRawAxis(5), m_left_front, m_right_front);
+      m_drive.drive(m_gamepad.getRawAxis(1), m_gamepad.getRawAxis(5), 1.);
     }
     else {
-      if(Error > 0)
-      {
-        m_left_front.set(ControlMode.PercentOutput, .3);
-        m_right_front.set(ControlMode.PercentOutput, .2);
-      }
-      else if(Error < 0)
-      {
-        m_left_front.set(ControlMode.PercentOutput, .2);
-        m_right_front.set(ControlMode.PercentOutput, .3);
-      }
-      else if(Error == 0)
-      {
-        m_left_front.set(ControlMode.PercentOutput, .2);
-        m_right_front.set(ControlMode.PercentOutput, .2);
-      }
-      else
-      {
-        m_left_front.set(ControlMode.PercentOutput, 0);
-        m_right_front.set(ControlMode.PercentOutput, 0);
-      }
-    }
-  }
-
-  private void tank_Drive(double joystick_left_y, double joystick_right_y, TalonSRX motor_left, TalonSRX motor_right)
-  {
-    // Impliment Deadzone
-    if(joystick_left_y < .02 && joystick_left_y > -.02)
-    {
-      joystick_left_y = 0;
-    }
-    if(joystick_right_y < .02 && joystick_right_y > -.02)
-    {
-      joystick_right_y = 0;
-    }
-
-    // Square joystick values
-    double updated_left = -joystick_left_y * Math.abs(joystick_left_y);
-
-    double updated_right = -joystick_right_y * Math.abs(joystick_right_y);
-
-    // Set left values
-    motor_left.set(ControlMode.PercentOutput, updated_left);
-    
-    // Set right values
-    motor_right.set(ControlMode.PercentOutput, updated_right);
-  }
-
-  private void ArduinoDrive()
-  {
-    byte[] output = m_arduino.read(m_arduino.getBytesReceived());
-    if(output.length == 0)
-    {
-      Error = 0;
-    }
-    else
-    {
-      Error = output[output.length-1];
-      System.out.println(Error);
+      m_pixy.lineFollowTalonSRX(m_leftFront, m_rightFront, .2);
     }
   }
 }
